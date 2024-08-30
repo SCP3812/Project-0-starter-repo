b9d150c1c6103c0698d1126efba5d2ae451f57e7
SCP3812 Wojak <mrisaacjgraham@gmail.com>
2024-08-30 16:45:43 +0000

    added import funcs as well as intro

---
+++ runner.py
@@ -0,0 +1,7 @@
+from character import Character
+from scene import Scene
+from item import Item
+from player import Player
+
+print("Welcome to MonkeyTime PlayFun Time Monkey Game by Games Incorporated")
+print("Introduction: You believe your life is literally a video game. Yet, however, when your mother forced you to go to woke therapy, the therapist said, 'Your life is not a game. It is a nightmare. Your mother constantly worries about your mental health.' Now, he has incarcerated you in one of his dark satanic mills, and you must escape back to your mother and save her from the woke mind virus infecting the world.")
